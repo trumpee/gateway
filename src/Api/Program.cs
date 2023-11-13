066ac2d1d7c12c437886b07b34e83e50f53297c2
Bardin08 <vbardin810@gmail.com>
2023-11-13 22:19:32 +0200

    Add a library for core logic

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,9 +1,11 @@
-﻿using FastEndpoints;
+﻿using Core;
+using FastEndpoints;
 using Infrastructure.Persistence;
 using Microsoft.AspNetCore.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddGatewayCore(builder.Configuration);
 builder.Services.AddPersistence(builder.Configuration);
 
 builder.Services.AddAuthorization(x =>
