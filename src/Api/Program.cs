8d2071c8fe732abdaa7be100d014600b690eb15c
Bardin08 <vbardin810@gmail.com>
2023-11-13 03:18:23 +0200

    Add FastEndpoints & configure it.
    FastEndpoints requires auth for all endpoints by default, so the only way to avoid auth for all endpoints is to configure a default auth policy to pass all requests.

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,4 +1,14 @@
+using FastEndpoints;
+using Microsoft.AspNetCore.Authorization;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddFastEndpoints();
+
+builder.Services.AddAuthorization(x =>
+    x.DefaultPolicy = new AuthorizationPolicyBuilder()
+        .RequireAssertion(_ => true)
+        .Build());
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -10,5 +20,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseAuthorization();
+app.UseFastEndpoints();
 
 app.Run();
