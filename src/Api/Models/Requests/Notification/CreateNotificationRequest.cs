f6c4db38906ab818c9336aeaf0f3a8368fc851c2
Bardin08 <vbardin810@gmail.com>
2023-11-22 01:31:18 +0200

    Add request & response models

--- src/Api/Models/Requests/Notification/CreateNotificationRequest.cs
+++ src/Api/Models/Requests/Notification/CreateNotificationRequest.cs
@@ -1,17 +1,5 @@
-﻿using Api.Models.Requests.Common;
-using Newtonsoft.Json;
+﻿namespace Api.Models.Requests.Notification;
 
-namespace Api.Models.Requests.Notification;
-
-internal record CreateNotificationRequest
+internal record CreateNotificationRequest : NotificationRequest
 {
-    public string? TemplateId { get; set; }
-    public PriorityRequest PriorityRequest { get; set; }
-
-    [JsonProperty("content")]
-    public ContentRequest? Content { get; set; }
-
-    public DateTimeOffset DeliveryTimestamp { get; set; }
-    public int RetryCount { get; set; }
-    public string? Status { get; set; }
 }
