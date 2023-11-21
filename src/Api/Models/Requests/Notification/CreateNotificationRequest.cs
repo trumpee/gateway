83e000b1c7bbec84c2699e225877c594745efe1c
Bardin08 <vbardin810@gmail.com>
2023-11-21 03:06:17 +0200

    Create notification request models

---
+++ src/Api/Models/Requests/Notification/CreateNotificationRequest.cs
@@ -0,0 +1,17 @@
+﻿using Api.Models.Requests.Common;
+using Newtonsoft.Json;
+
+namespace Api.Models.Requests.Notification;
+
+internal record CreateNotificationRequest
+{
+    public string? TemplateId { get; set; }
+    public PriorityRequest PriorityRequest { get; set; }
+
+    [JsonProperty("content")]
+    public ContentRequest Content { get; set; }
+
+    public DateTimeOffset DeliveryTimestamp { get; set; }
+    public int RetryCount { get; set; }
+    public string? Status { get; set; }
+}
---
+++ src/Api/Models/Requests/Notification/RecipientRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Api.Models.Requests.Notification;
+
+internal class RecipientRequest
+{
+    public string? UserId { get; set; }
+    public string? Channel { get; set; }
+}
