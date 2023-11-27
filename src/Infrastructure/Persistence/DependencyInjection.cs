9b0a3e80d0b4c17df48f7eb45a6c4dd0e38f58dd
Bardin08 <vbardin810@gmail.com>
2023-11-28 01:48:22 +0200

    Add MassTransit persistence

--- src/Infrastructure/Persistence/DependencyInjection.cs
+++ src/Infrastructure/Persistence/DependencyInjection.cs
@@ -1,8 +1,12 @@
-﻿using Infrastructure.Persistence.Mongo.Abstractions;
+﻿using System.Runtime.InteropServices.Marshalling;
+using Infrastructure.Persistence.MassTransit;
+using Infrastructure.Persistence.Mongo.Abstractions;
 using Infrastructure.Persistence.Mongo.Configurations;
+using Infrastructure.Persistence.Mongo.Entities.Notifications;
 using Infrastructure.Persistence.Mongo.Repositories;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Trumpee.MassTransit;
 
 namespace Infrastructure.Persistence;
 
@@ -10,14 +14,22 @@ public static class DependencyInjection
 {
     public static void AddPersistence(this IServiceCollection services, IConfiguration config)
     {
-        services.AddScoped<ITemplatesRepository, TemplatesRepository>();        
-        services.AddScoped<INotificationsRepository, NotificationsRepository>();        
-
         services.AddMongoDb(config);
+        services.AddMassTransit(config);
     }
 
     private static void AddMongoDb(this IServiceCollection services, IConfiguration config)
     {
+        services.AddScoped<ITemplatesRepository, TemplatesRepository>();        
+        services.AddScoped<INotificationsRepository, NotificationsRepository>();        
+
         services.Configure<MongoDbOptions>(config.GetSection(MongoDbOptions.ConfigurationSectionName));
+    }
+
+    private static void AddMassTransit(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddConfiguredMassTransit(config);
+
+        services.AddScoped<IMassTransitClient<Notification>, MassTransitClient<Notification>>();
     }
 }
---
+++ src/Infrastructure/Persistence/MassTransit/MassTransitClient.cs
@@ -0,0 +1,24 @@
+﻿using MassTransit;
+
+namespace Infrastructure.Persistence.MassTransit;
+
+public class MassTransitClient<TMessage> : IMassTransitClient<TMessage> where TMessage : class
+{
+    private readonly ISendEndpointProvider _endpointProvider;
+
+    public MassTransitClient(ISendEndpointProvider endpointProvider)
+    {
+        _endpointProvider = endpointProvider;
+    }
+
+    public async Task SendMessages(List<TMessage> messages)
+    {
+        var endpoint = await _endpointProvider.GetSendEndpoint(new Uri("queue:validation"));
+        await endpoint.SendBatch(messages);
+    }
+}
+
+public interface IMassTransitClient<TMessage>
+{
+    Task SendMessages(List<TMessage> messages);
+}
