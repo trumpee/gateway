5a9d36ec3094d8c281a476beaa955b96c6a8e62e
Bardin08 <vbardin810@gmail.com>
2023-12-03 19:34:22 +0200

    Add MassTransit contracts mappers

---
+++ src/Core/Mappers/External/ContentNapper.cs
@@ -0,0 +1,25 @@
+﻿using Core.Models.Common;
+using Trumpee.MassTransit.Messages.Notifications;
+
+namespace Core.Mappers.External;
+
+internal static class ContentNapper
+{
+    internal static Content ToContent(ContentDto dto)
+    {
+        var variables = new Dictionary<string, Variable>();
+        if (dto.Variables is { Count: > 0 })
+        {
+            variables = dto.Variables.Values
+                .Select(VariableMapper.ToVariable)
+                .ToDictionary(x => x.Name, x => x);
+        }
+
+        return new Content
+        {
+            Subject = dto.Subject!,
+            Body = dto.Body!,
+            Variables = variables
+        };
+    }
+}
---
+++ src/Core/Mappers/External/RecipientMapper.cs
@@ -0,0 +1,14 @@
+﻿using Core.Models.Notifications;
+using Trumpee.MassTransit.Messages.Notifications;
+
+namespace Core.Mappers.External;
+
+internal static class RecipientMapper
+{
+    internal static Recipient ToRequest(RecipientDto d) =>
+        new()
+        {
+            UserId = d.UserId!,
+            Channel = d.Channel!
+        };
+}
