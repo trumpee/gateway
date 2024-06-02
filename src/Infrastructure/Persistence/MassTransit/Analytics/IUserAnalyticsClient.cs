05bb3efec47a5fc87ec0b251f646eee9a4c877fb
Bardin08 <vbardin810@gmail.com>
2024-06-02 15:39:40 +0300

    Add a user analytics client

---
+++ src/Infrastructure/Persistence/MassTransit/Analytics/IUserAnalyticsClient.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure.Persistence.MassTransit.Analytics;
+
+public interface IUserAnalyticsClient
+{
+    Task SendUserSignIn(string email, CancellationToken ct);
+    Task SendUserSignUp(string userId, string email, string name, string roles, CancellationToken ct);
+    Task SendUserAction(string email, string userAction, string usedRole, string affectedData, CancellationToken ct);
+}
--- src/Infrastructure/Persistence/MassTransit/Analytics/UserAnalyticsClient.cs
+++ src/Infrastructure/Persistence/MassTransit/Analytics/UserAnalyticsClient.cs
@@ -1,36 +1,41 @@
 ﻿using MassTransit;
 using Trumpee.MassTransit;
 using Trumpee.MassTransit.Messages.Analytics.Users;
+using Trumpee.MassTransit.Messages.Analytics.Users.Payloads;
 
 namespace Infrastructure.Persistence.MassTransit.Analytics;
 
 public class UserAnalyticsClient(ISendEndpointProvider sendEndpointProvider) : IUserAnalyticsClient
 {
     private readonly ISendEndpointProvider _endpointProvider = sendEndpointProvider;
-    
+
+    private static readonly string SignUpQueue = QueueNames.Analytics.Users(typeof(UserSignUpPayload));
+    private static readonly string SignIpQueue = QueueNames.Analytics.Users(typeof(UserSignInPayload));
+    private static readonly string UserActionQueue = QueueNames.Analytics.Users(typeof(UserActionPayload));
+
     public async Task SendUserSignIn(string email, CancellationToken ct)
     {
         var analyticsEvent = User.SignIn("Trumpee Gateway", email);
-        await SendEvent(analyticsEvent, ct);
+        await SendEvent(analyticsEvent, SignIpQueue, ct);
     }
-    
+
     public async Task SendUserSignUp(string userId,
         string email, string name, string roles, CancellationToken ct)
     {
         var analyticsEvent = User.SignUp("Trumpee Gateway", userId, email, name, roles);
-        await SendEvent(analyticsEvent, ct);
+        await SendEvent(analyticsEvent, SignUpQueue, ct);
     }
-    
+
     public async Task SendUserAction(string email,
         string userAction, string usedRole, string affectedData, CancellationToken ct)
     {
         var analyticsEvent = User.Action("Trumpee Gateway", email, userAction, usedRole, affectedData);
-        await SendEvent(analyticsEvent, ct);
+        await SendEvent(analyticsEvent, UserActionQueue, ct);
     }
-    
-    private async Task SendEvent<T>(T analyticsEvent, CancellationToken ct) where T : class
+
+    private async Task SendEvent<T>(T analyticsEvent, string queueName, CancellationToken ct) where T : class
     {
-        var endpoint = await _endpointProvider.GetSendEndpoint(new Uri(QueueNames.Analytics.Users));
+        var endpoint = await _endpointProvider.GetSendEndpoint(new Uri(queueName));
         await endpoint.Send(analyticsEvent, ct);
     }
 }
